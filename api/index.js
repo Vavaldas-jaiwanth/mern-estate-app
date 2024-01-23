590857028f6a112be5ab70728d7d2b5972e14cd7
vavaldas jaiwanth <jaiwanthvavaldas@gmail.com>
2024-01-23 16:14:00 +0530

    db connection

--- api/index.js
+++ api/index.js
@@ -1,4 +1,30 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+const mongoURI = process.env.URL;
+
+
+mongoose
+  .connect(mongoURI)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
+
+// mongoose
+// .connect('mongodb+srv://vavaldasjaiwanth:jaiv9164@mern-estate.wpslkfy.mongodb.net/?retryWrites=true&w=majority')
+// .then(()=>{
+//     console.log("Connected to MongoDB!!");
+// })
+// .catch((err)=>{
+//     console.log(err);
+// })
+
 const app=express();
 
 app.listen(3000,()=>{
